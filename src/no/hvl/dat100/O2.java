1832bb03d0370b04d6eb973727da20edb5610a11
602604 <602604@stud.hvl.no>
2021-09-13 18:41:56 +0200

    oppgaver uke 36

--- src/no/hvl/dat100/O2.java
+++ src/no/hvl/dat100/O2.java
@@ -5,39 +5,44 @@ import javax.swing.JOptionPane;
 public class O2 {
 
 	public static void main(String[] args) {
-		String input = JOptionPane.showInputDialog("Gi tall");
-		int karakter= Integer.parseInt(input);
-		
-		
-		if(karakter<39) {
+				
+		 for (int i = 0; i < 10; i++) {
 			
-			System.out.println("F");
-		}
-		else if (karakter>=40 && karakter<=49) {
-			
-			System.out.println("E");
-		}
-		else if (karakter>=50 && karakter<=59) {
-			
-			System.out.println("D");
-		}
-		else if (karakter>=60 && karakter<=79) {
-			
-			System.out.println("C");
-		}
-		else if (karakter>=80 && karakter<=89) {
-			
-			System.out.println("B");
-		}
-		else if (karakter>=90 && karakter<=100){
-			
-			System.out.println("A");
-		}
-		else {
-			
-			System.out.println("ugyldig poengsum");
-		}
-
+			 String poengsumm = JOptionPane.showInputDialog("student poegner");
+			 int karakter= Integer.parseInt(poengsumm);
+			 
+			 if(karakter<39 && karakter>=0) {
+					
+					System.out.println("F");
+				}
+				else if (karakter>=40 && karakter<=49) {
+					
+					System.out.println("E");
+				}
+				else if (karakter>=50 && karakter<=59) {
+					
+					System.out.println("D");
+				}
+				else if (karakter>=60 && karakter<=79) {
+					
+					System.out.println("C");
+				}
+				else if (karakter>=80 && karakter<=89) {
+					
+					System.out.println("B");
+				}
+				else if (karakter>=90 && karakter<=100){
+					
+					System.out.println("A");
+				}
+				else {
+					
+					System.out.println("ugyldig poengsum");
+					 
+				}
+			 System.out.println("student " + i);
+		 }
+		 
 	}
 
 }
--- src/no/hvl/dat100/O3.java
+++ src/no/hvl/dat100/O3.java
@@ -1,18 +1,22 @@
 package no.hvl.dat100;
 
+
+
 import javax.swing.JOptionPane;
 
 public class O3 {
 
 	public static void main(String[] args) {
 		
-		String input = JOptionPane.showInputDialog("Gi tall");
-		int n = Integer.parseInt(input);
-		
 		int i;
-		for (i = 1; i <= n; i++) {
-			
-			System.out.println(n);
+		int a = 1;
+		
+		String verdien = JOptionPane.showInputDialog("Give nummber");
+		int heltall = Integer.parseInt(verdien);
+		
+		for (i = 1; i <= heltall; i++) {
+			a = a * i;
+			System.out.println(a);
 		}
 
 	}
